22ff1a78c268e86362fd19b5b2648bee22125d05
Nate Wang <supnate@gmail.com>
2016-08-09 00:22:29 +0800

    Update storage app id.

--- src/storage/leanInit.js
+++ src/storage/leanInit.js
@@ -1,7 +1,7 @@
 import lean from 'leancloud-storage';
 
-const APP_ID = 'QdssNr8xprXX1OHF2Hb458qz-gzGzoHsz';
-const APP_KEY = 'LOqvRUIBaBGq2gMUd03juHxA';
+const APP_ID = 'BmwY0Jdi9dDahDPkoXr1XYUn-gzGzoHsz';
+const APP_KEY = 'DJc54xT7XbM84wsraTUGWavn';
 lean.init({
   appId: APP_ID,
   appKey: APP_KEY
