d92c3d5292ec47dacfe4f9a875e57505cbf3448d
Nate <nate.wang@sap.com>
2016-10-24 11:22:18 +0800

    Remove unecessary actions import.

--- src/features/comment/index.js
+++ src/features/comment/index.js
@@ -1,9 +1,7 @@
-import * as actions from './actions';
 import CommentForm from './CommentForm';
 import CommentList from './CommentList';
 
 export {
-  actions,
   CommentForm,
   CommentList,
 };
--- src/features/topic/index.js
+++ src/features/topic/index.js
@@ -1,10 +1,8 @@
-import * as actions from './actions';
 import EditPage from './EditPage';
 import ListPage from './ListPage';
 import ViewPage from './ViewPage';
 
 export {
-  actions,
   EditPage,
   ListPage,
   ViewPage,
